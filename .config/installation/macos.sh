835060fa9f84c6f7c0db6503c3cc19e9d2d91e5a
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-05-19 20:55:26 -0400

    code settings for MacOS

--- .config/installation/macos.sh
+++ .config/installation/macos.sh
@@ -42,5 +42,12 @@ brew cask install $PACKS
 
 sh -c 'curl -fLo "$XDG_DATA_HOME/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+#
+# vscode settings
+#
+
+rm $HOME/Library/Application\ Support/Code/User/settings.json
+ln $XDG_CONFIG_HOME/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings.json   
+
 # reset git config
 GIT_CONFIG="$gcfg"
