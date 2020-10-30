462e602e1b650dc6229a81c423001e3c78637532
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-10-30 14:47:16 -0400

    Add and tweak wm, network, volume, and battery bar modules

--- .config/lemonbar/modules.sh
+++ .config/lemonbar/modules.sh
@@ -3,11 +3,45 @@
 SLEEP_TIME=30
 
 bspwm() {
-  echo -e $(bspc query -D --names -d)
+  ws=$(bspc query -D --names -d)
+  case $ws in
+    # TODO replace with icons
+    I) echo -e "I Development" ;;
+    II) echo -e "II Browsing" ;;
+    III) echo -e "III Gaming" ;;
+    IV) echo -e "IV Chat" ;;
+    V) echo -e "V Media" ;;
+    # ...
+    IX) echo -e "IX News" ;;
+    X) echo -e "X Music" ;;
+    # *) ;;
+  esac
+}
+
+network() {
+  case $OS in
+    *) echo -e "net not yet implemented" ;;
+  esac
+}
+
+volume() {
+  case $OS in
+    $OS_FREEBSD)
+      # just using the left channel
+      vol=$(mixer vol | awk '{print $NF}' | cut -d: -f1)
+      echo -e "vol ${vol}%"
+      ;;
+    *) echo -e "vol not yet implemented" ;;
+  esac
 }
 
 battery() {
-  echo -e $(apm | awk '/Remaining battery/ {print $4;exit}')
+  case $OS in
+    $OS_FREEBSD)
+      echo -e "bat $(apm | awk '/Remaining battery/ {print $4;exit}')"
+      ;;
+    *) echo -e "bat not yet implemented" ;;
+  esac
 }
 
 clock() {
@@ -17,7 +51,7 @@ clock() {
 while :; do
   LEFT="$(bspwm)"
   CENTER=""
-  RIGHT="$(battery) $(clock)"
+  RIGHT="$(network) $(volume) $(battery) $(clock)"
 
   echo -e "%{F$BAR_FG}%{l}$LEFT%{c}$CENTER%{r}$RIGHT"
   sleep $SLEEP_TIME &
