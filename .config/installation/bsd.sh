6bd7dc8dade5c751bc2e03a63904588cab7a10ef
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-10-31 09:25:03 -0400

    Switch from lemonbar to lemonbar-xft

--- .config/installation/bsd.sh
+++ .config/installation/bsd.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+TMP_DIR=/tmp
+
 #
 # install official repository packages
 #
 
 sudo pkg install bspwm sxhkd
-sudo pkg install lemonbar
+
+git clone https://github.com/krypt-n/bar.git $TMP_DIR/lemonbar-xft
+cp -f $XDG_CONFIG_HOME/lemonbar/Makefile $TMP_DIR/lemonbar-xft/
+cd $TMP_DIR/lemonbar-xft
+sudo gmake clean install
 
 sudo pkg install sourcecodepro-ttf
 sudo cp /usr/local/share/fonts/SourceCodePro/*.ttf /usr/local/share/fonts/TTF/
--- .config/lemonbar/main.sh
+++ .config/lemonbar/main.sh
@@ -8,4 +8,4 @@ fi
 
 $XDG_CONFIG_HOME/lemonbar/modules.sh | $BAR -p \
   -g "${W_WIDTH}x14+${W_GAPS}+${W_GAPS}" \
-  -f "-adobe-source code pro-medium-r-normal--0-0-0-0-m-0-iso8859-1" &
+  -f "Source Code Pro" &
