d4c52deb16cb6bfa29c8bdfe0ff1906f5b25970a
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-11-12 15:36:39 -0500

    Add mpd and ncmpcpp to BSD

--- .config/installation/bsd.sh
+++ .config/installation/bsd.sh
@@ -55,6 +55,16 @@ PACKS="${PACKS} feh mpv"
 PACKS="${PACKS} slop"
 PACKS="${PACKS} yarn"
 
+PACKS="${PACKS} ncmpcpp"
+
+# mpd
+git clone "https://github.com/MusicPlayerDaemon/MPD.git" "${TMP_DIR}/mpd"
+cd "${TMP_DIR}/mpd"
+git checkout eb9f5339b683d037ab3224e362071e22991e0e83
+meson . output/release
+meson configure output/release -Dcpp_args="-I/usr/local/include"
+sudo ninja -C output/release install
+
 PACKS="${PACKS} newsboat"
 
 PACKS="${PACKS} textlive-full"
