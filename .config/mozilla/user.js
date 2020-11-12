886e78a2fb94beaa1d3759fb8fb4a455c8a47468
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-11-12 18:02:04 -0500

    Add vital preferences to user.js and convert to template

--- .config/mozilla/user.js
+++
@@ -1,6 +0,0 @@
-user_pref("gfx.webrender.all", true);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
-user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
-user_pref("toolkit.telemetry.enabled", false);
-user_pref("browser.urlbar.placeholderName", "duckduckgo");
