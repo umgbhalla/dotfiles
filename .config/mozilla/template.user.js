6108c82ee8a6933af1096d0ad01ad69c6689eca7
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-11-17 21:12:41 -0500

    Fix FF strange color mishaps and add configuration for most properties in user.js

--- .config/mozilla/template.user.js
+++ .config/mozilla/template.user.js
@@ -1,3 +1,70 @@
+// startup
+user_pref("browser.sessionstore.resume_session_once", true);
+user_pref("browser.sessionstore.resume_from_crash", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
+
+// tabs
+user_pref("browser.ctrlTab.recentlyUsedOrder", false);
+
+// downloads
+user_pref("browser.download.dir", "${HOME}/Downloads");
+user_pref("browser.download.useDownloadDir", "true");
+
+// TODO application actions
+
+// browsing
+user_pref("general.smoothScroll", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+
+// new windows and tabs
+user_pref("startup.homepage_welcome_url", "about:welcome");
+user_pref("browser.startup.homepage", "about:home");
+
+// firefox home content
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.sections", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts", false);
+
+// TODO
+// default search engine can NO LONGER be set in about:config due to
+// third-party plugins setting the default search engine
+
+// TODO search shortcuts
+
+// logins and passwords
+user_pref("signon.rememberSignons", false);
+
+// forms and autofill
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+
+// address bar
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+
+// permissions
+// popups
+user_pref("dom.disable_open_during_load", false);
+
+// firefox data and collection use - see telemetry
+
+// deceptive content and dangerous software protection
+// stop monitoring downloads!
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+
+//
+// miscellaneous
+//
+
+// about:config warning
+user_pref("browser.aboutConfig.showWarning", false);
+
 // opengl
 user_pref("gfx.webrender.all", true);
 
@@ -8,25 +75,57 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 
-// telemetry !!!
+// telemetry
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 user_pref("toolkit.telemetry.enabled", false);
-
-// downloads
-user_pref("browser.download.dir", "${HOME}/Downloads");
-user_pref("browser.download.useDownloadDir", "true");
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("devtools.onboarding.telemetry.logged", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("devtools.onboarding.telemetry.logged", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.sessions.current.clean", true);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.sessions.current.clean", true);
+user_pref("services.sync.prefs.sync.app.shield.optoutstudies.enabled", false);
+user_pref("services.sync.prefs.sync.browser.discovery.enabled", false);
+user_pref("app.normandy.enabled", false);
 
 // syncing
 user_pref("services.sync.declinedEngines", "passwords,addresses,creditcards");
 
-// autofill
-user_pref("dom.payments.defaults.saveAddress", false);
-user_pref("extensions.formautofill.addresses.enabled", false);
-user_pref("dom.payments.defaults.saveCreditCard", false);
-user_pref("extensions.formautofill.creditCards.enabled", false);
-
 // UI customization
-user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"home-button\",\"urlbar-container\",\"downloads-button\",\"library-button\",\"sidebar-button\",\"fxa-toolbar-menu-button\",\"_testpilot-containers-browser-action\",\"adblockultimate_adblockultimate_net-browser-action\",\"_react-devtools-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"managed-bookmarks\",\"personal-bookmarks\"]},\"seen\":[\"developer-button\",\"_testpilot-containers-browser-action\",\"adblockultimate_adblockultimate_net-browser-action\",\"_react-devtools-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":16,\"newElementCount\":3}");
+//       \"library-button\",
+user_pref("browser.uiCustomization.state", "{
+  \"placements\": {
+    \"nav-bar\": [
+      \"urlbar-container\",
+      \"downloads-button\",
+      \"_testpilot-containers-browser-action\",
+      \"adblockultimate_adblockultimate_net-browser-action\",
+      \"_react-devtools-browser-action\"
+    ],
+    \"PersonalToolbar\": []
+  },
+  \"currentVersion\": 16
+}");
 user_pref("browser.display.background_color", "${FF_BG}");
 user_pref("browser.display.foreground_color", "${FF_FG}");
 
@@ -37,5 +136,21 @@ user_pref("devtools.editor.keymap", "vim"); // bindings for all devtools file ed
 // media
 user_pref("dom.media.autoplay.autoplay-policy-api", false);
 
+// pocket
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.pocket.onSaveRecs", false);
+
+// downloads
+// display download panel on download
+user_pref("browser.download.panel.shown", true);
+// always display download icon in bar
+user_pref("browser.download.autohideButton", false);
+
 // extensions
 user_pref("extensions.update.enabled", false);
+user_pref("extensions.logging.enabled", false);
+user_pref("extensions.update.autoUpdateEnabled", false);
+// TODO extensions.webextensions.uuids
+// extensions. enabledAddons
+// user_pref("extensions.webextensions.uuids", "{\"default-theme@mozilla.org\":\"b0773b60-c95e-4349-a479-ed229fda20c8\",\"ddg@search.mozilla.org\":\"c4ab20be-9bda-4f88-bed8-26fa5145e8b7\",\"@testpilot-containers\":\"217140a1-0821-4728-b48a-5b5a170d01bf\"}");
+// user_pref("extensions.webextensions.uuids", "{\"doh-rollout@mozilla.org\":\"af86dde3-3012-4b52-b226-932f40771bc2\",\"formautofill@mozilla.org\":\"b77278e9-1462-4226-b2f6-38e5a8e0f4e6\",\"screenshots@mozilla.org\":\"ba8a7714-a1fe-478d-bbea-3d832d5f609c\",\"webcompat-reporter@mozilla.org\":\"f2c7b270-8853-4958-bb73-164170ab0ac9\",\"webcompat@mozilla.org\":\"893e226b-97f5-4812-9689-47c825241d54\",\"default-theme@mozilla.org\":\"b0773b60-c95e-4349-a479-ed229fda20c8\",\"google@search.mozilla.org\":\"9e2fd104-4352-4baf-8531-a3679ed1656b\",\"amazondotcom@search.mozilla.org\":\"90ba61bc-77ca-463e-8500-193ee8f4c715\",\"wikipedia@search.mozilla.org\":\"1371e244-86f7-4820-8db7-bbab04440530\",\"bing@search.mozilla.org\":\"6f78b06c-fbff-478c-9880-08ba366bf328\",\"ddg@search.mozilla.org\":\"c4ab20be-9bda-4f88-bed8-26fa5145e8b7\",\"ebay@search.mozilla.org\":\"8f3829c7-abb2-4b9c-b4fc-ac2681cdfb39\",\"@testpilot-containers\":\"217140a1-0821-4728-b48a-5b5a170d01bf\"}");
