8985d0b6085b5b389b47858414876055a29cfb7f
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-11-21 01:31:29 -0500

    WIP remove all un-essentials from installation script

--- .config/install/arch.sh
+++ .config/install/arch.sh
@@ -1,84 +1,110 @@
 #!/bin/sh
 
-# unset git config to prevent yay errors
-gcfg="$GIT_CONFIG"
-unset GIT_CONFIG
-
-git clone "https://aur.archlinux.org/yay.git" "$TMP_DIR/yay"
-cd "$TMP_DIR/yay" && makepkg -si
+# try paru?
+# commented to incentivize manual compilation
+#
+# git clone "https://aur.archlinux.org/yay.git" "$TMP_DIR/yay"
+# cd "$TMP_DIR/yay" && makepkg -si
 
 PACKS=""
 
-PACKS="${PACKS} xorg-server xorg-xinit"
-PACKS="${PACKS} bspwm sxhkd"
-PACKS="${PACKS} libxft"
-PACKS="${PACKS} neovim nodejs ripgrep"
-PACKS="${PACKS} firefox"
-PACKS="${PACKS} fzf"
-PACKS="${PACKS} feh"
-PACKS="${PACKS} ttf-liberation wqy-zenhei"
-PACKS="${PACKS} redshift"
-PACKS="${PACKS} vifm"
-PACKS="${PACKS} xorg-xsetroot hsetroot"
-PACKS="${PACKS} xclip"
-PACKS="${PACKS} htop"
+# documentation
 PACKS="${PACKS} man"
+
+# xorg
+PACKS="${PACKS} xorg-server xorg-xinit"
+
+# git clone "https://github.com/baskerville/bspwm.git" "${TMP_DIR}/bspwm"
+# cd "${TMP_DIR}/bspwm"
+# git checkout 51983994fa078b7a7ce4fe796d4625a4ad1e4251
+# sudo make clean install
+
+# wm
+PACKS="${PACKS} bspwm sxhkd"
+# PACKS="${PACKS} libxft"
+# editor
+PACKS="${PACKS} neovim nodejs ripgrep"
+# fonts
+PACKS="${PACKS} ttf-liberation adobe-source-code-pro-fonts wqy-zenhei"
+# browser
+PACKS="${PACKS} firefox"
+# fuzzy finder
+PACKS="${PACKS} fzf"
+# image viewer
+PACKS="${PACKS} feh"
+# screenshot utility
 PACKS="${PACKS} slop"
+# redshift
+PACKS="${PACKS} redshift"
+# file manager
+PACKS="${PACKS} vifm"
+#
+PACKS="${PACKS} xorg-xsetroot hsetroot"
+# clipboard
+PACKS="${PACKS} xclip"
+# system profiler
+PACKS="${PACKS} htop"
+# ssh
 PACKS="${PACKS} openssh"
+# media player
 PACKS="${PACKS} mpv"
+# rss reader
 PACKS="${PACKS} newsboat"
-PACKS="${PACKS} yarn"
+# PACKS="${PACKS} yarn"
+# pdf viewer utility
 PACKS="${PACKS} zathura-pdf-mupdf"
 
+# media manipulation
 PACKS="${PACKS} ffmpeg"
 # PACKS="${PACKS} kdenlive"
 
-PACKS="${PACKS} pulseaudio pulseaudio-alsa pamixer pavucontrol"
-PACKS="${PACKS} mpd ncmpcpp"
+# PACKS="${PACKS} pulseaudio pulseaudio-alsa pamixer pavucontrol"
+# PACKS="${PACKS} mpd ncmpcpp"
 
-PACKS="${PACKS} texlive-most biber"
+# PACKS="${PACKS} texlive-most biber"
 
-PACKS="${PACKS} figlet"
+# PACKS="${PACKS} figlet"
 
 sudo pacman -S --needed $PACKS # cannot be quoted
 
-AUR="mmv"
+# AUR="mmv"
 
-AUR="$AUR picom-ibhagwan-git"
-AUR="$AUR adobe-source-code-pro-fonts"
-AUR="$AUR zathura-git"
-AUR="$AUR youtube-dl"
+# AUR="$AUR picom-ibhagwan-git"
+# AUR="$AUR adobe-source-code-pro-fonts"
+# AUR="$AUR zathura-git"
+# AUR="$AUR youtube-dl"
 
-AUR="$AUR xgetres"
+# AUR="$AUR xgetres"
 
-yay -S --needed --batchinstall $AUR # cannot be quoted
+# yay -S --needed --batchinstall $AUR # cannot be quoted
 
-# firefox setup
-$DETEMPLATE "${XDG_CONFIG_HOME}/mozilla/template.user.js"
-firefox -CreateProfile "${FF_PROFILE}"
-baseDir="${HOME}/.mozilla/firefox"
-profileDir="$(ls "${baseDir}" | grep ".${FF_PROFILE}")"
-ffDir="${baseDir}/${profileDir}"
-rm -f "${ffDir}/chrome" >/dev/null
-rm -f "${ffDir}/user.js" >/dev/null
-ln -sf "${XDG_CONFIG_HOME}/mozilla/profile/chrome" "${ffDir}/chrome"
-ln -sf "${XDG_CONFIG_HOME}/mozilla/user.js" "${ffDir}/user.js"
+# browser profile
+# $DET "${XDG_CONFIG_HOME}/mozilla/template.user.js"
+# firefox -CreateProfile "${FF_PROFILE}"
+# baseDir="${HOME}/.mozilla/firefox"
+# profileDir="$(ls "${baseDir}" | grep ".${FF_PROFILE}")"
+# ffDir="${baseDir}/${profileDir}"
+# rm -f "${ffDir}/chrome" >/dev/null
+# rm -f "${ffDir}/user.js" >/dev/null
+# ln -sf "${XDG_CONFIG_HOME}/mozilla/profile/chrome" "${ffDir}/chrome"
+# ln -sf "${XDG_CONFIG_HOME}/mozilla/user.js" "${ffDir}/user.js"
 
 # lemonbar
-git clone "https://github.com/krypt-n/bar.git" "${TMP_DIR}/lemonbar-xft"
-cd "${TMP_DIR}/lemonbar-xft" && sudo make clean install
+git clone "https://github.com/krypt-n/bar.git" "${TMP_DIR}/lemonbar"
+cd "${TMP_DIR}/lemonbar"
+sudo make clean install
 
 # relink /bin/sh
-sudo ln -sfT mksh /bin/sh
+sudo ln -sf mksh /bin/sh
 
 # suckless
 $SBUILD "st"
 $SBUILD "herbe"
 
 # gtk theme
-$DETEMPLATE "${XDG_CONFIG_HOME}/${THEME}/gtk-3.0/template.colors.css"
+$DET "${XDG_CONFIG_HOME}/${THEME}/gtk-3.0/template.gtk.css"
 sudo mkdir -p "$GTK_THEME_DIR"
-sudo ln -sf "$XDG_CONFIG_HOME/$THEME" "$GTK_THEME_DIR/$THEME"
+sudo ln -sf "${XDG_CONFIG_HOME}/${THEME}" "${GTK_THEME_DIR}/${THEME}"
 
 # font(s)
 mkdir -p "$FONT_DIR"
@@ -89,15 +115,12 @@ fc-cache -f -v
 sudo ln -sf "$XDG_CONFIG_HOME/xorg.conf.d/30-touchpad.conf" "/etc/X11/xorg.conf.d/30-touchpad.conf"
 
 # login prompt
-if command -v "figlet"; then
-  issue="$(cat "/etc/hostname" | figlet -k | sed 's/\\/\\\\\\/g')"
-  issuestatus="(\\l) \\s \\\r \\\t"
-  # -e interprets \n as a new line character
-  echo -e "${issue}\n${issuestatus}" | sudo tee "/etc/issue" > /dev/null
-fi
+# if command -v "figlet"; then
+#   issue="$(cat "/etc/hostname" | figlet -k | sed 's/\\/\\\\\\/g')"
+#   issuestatus="(\\l) \\s \\\r \\\t"
+#   # -e interprets \n as a new line character
+#   echo -e "${issue}\n${issuestatus}" | sudo tee "/etc/issue" > /dev/null
+# fi
 
 # motd
 echo "" | sudo tee "/etc/motd"
-
-# reset git config
-GIT_CONFIG="$gcfg"
