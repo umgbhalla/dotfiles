e764031bb33e3ae6253a391ef79f2ca28ed8e045
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-12-09 11:06:45 -0500

    Implement urlencoding in C and add devour to yt script

--- .config/ytui/main.c
+++ .config/ytui/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* ansi color codes */
 const char* RED = "\033[31m";
@@ -10,7 +12,6 @@ unsigned int getLength(char* str) {
   while (str[c] != '\0') c++;
   return c;
 }
-
 
 /* getSearchStr makes the assumption that a search */
 /* string exists to begin with */
@@ -32,9 +33,53 @@ char* getSearchStr(int argc, char* argv[]) {
 }
 
 char* urlEncode(char* url) {
-  /* unsigned int bufferLen = getLength(url) * 3; */
-  /* char buffer[bufferLen]; */
-  return url;
+  /* at a maximum, every character could be converted */
+  /* to a three character string */
+  unsigned int bufferLen = getLength(url) * 3;
+  char* buffer = (char*) malloc(sizeof(char) * bufferLen);
+  /* buffer char counter */
+  unsigned int bc = 0;
+  /* url char counter */
+  unsigned int c = 0;
+
+  while (url[c] != '\0') {
+    /* printf("char is %c\n", url[c]); */
+
+    switch (url[c]) {
+      case ' ': {
+        *(buffer+bc) = '%';
+        bc++;
+        *(buffer+bc) = '2';
+        bc++;
+        *(buffer+bc) = '0';
+        break;
+      }
+      case '\'': {
+        *(buffer+bc) = '%';
+        bc++;
+        *(buffer+bc) = '9';
+        bc++;
+        *(buffer+bc) = '1';
+        break;
+      }
+      case '%': {
+        *(buffer+bc) = '%';
+        bc++;
+        *(buffer+bc) = '2';
+        bc++;
+        *(buffer+bc) = '5';
+        break;
+      }
+      default: {
+        *(buffer+bc) = url[c];
+      }
+    }
+
+    bc++;
+    c++;
+  }
+
+  return buffer;
 }
 
 int main(int argc, char* argv[]) {
@@ -44,7 +89,27 @@ int main(int argc, char* argv[]) {
   }
 
   char* searchStr = urlEncode(getSearchStr(argc, argv));
+  const char* FQDN = "https://www.youtube.com";
+  const char* RESOURCE_SEARCH_RESULTS = "/results?search_query=";
 
-  printf("searchStr is '%s'\n", searchStr);
+  unsigned int searchStrLen = getLength(searchStr);
+  unsigned int domainLen = getLength((char*) FQDN);
+  unsigned int resourceLen = getLength((char*) RESOURCE_SEARCH_RESULTS);
+
+  char* searchUrl = (char*) malloc(sizeof(char)* (searchStrLen + domainLen + resourceLen));
+
+  strcat(searchUrl, FQDN);
+  strcat(searchUrl, RESOURCE_SEARCH_RESULTS);
+  strcat(searchUrl, searchStr);
+
+  printf("url is %s\n", searchUrl);
+
+  free(searchStr);
+  free(searchUrl);
+
+  /* GET /http-get-and-post-methods-example-in-c/ HTTP/1.1 */
+  /* Host: www.aticleworld.com */
+  /* Content-Type: text/plain */
+
   return 0;
 }
