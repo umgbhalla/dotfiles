a72b261847faff1aca28277a199fa5881912658c
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-04-28 03:58:28 -0400

    Create battery script

--- .config/polybar/scripts/battery.sh
+++ .config/polybar/scripts/battery.sh
@@ -3,29 +3,11 @@
 charging=""
 discharging=""
 
-LINUX_BAT="/sys/class/power_supply/BAT0"
-
-if command -v "apm" > "$NULL"; then
-  pow="$(apm)"
-
-  bat="$(echo "$pow" | awk '/Battery state/ {gsub("%","");print $4;exit}')"
-
-  status="$(echo "$pow" | awk '/A\/C adapter state/ {print $4}')"
-  if [ "$status" = "connected" ]; then status="$charging"
-  else status="$discharging"
-  fi
-
-  echo "${status} ${bat}"
-
-elif [ -e "$LINUX_BAT" ]; then
-  bat="$(cat "/sys/class/power_supply/BAT0/capacity" 2> "$NULL")"
-
-  status="$(cat "/sys/class/power_supply/BAT0/status")"
-  if [ "$status" = "Charging" ]; then status="$charging"
-  else status="$discharging"
-  fi
-
-  echo "${status} ${bat}"
-else
-  echo "$charging"
+status="$charging"
+if [ "$(bat status)" == "discharging" ]; then
+  status="$discharging"
 fi
+
+bat="$(bat get)"
+
+echo "${status} ${bat}"
