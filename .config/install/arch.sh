1551538da962b8d49504899722bb94610b3e0d03
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-04-30 11:32:58 -0400

    Complete arch install

--- .config/install/arch.sh
+++ .config/install/arch.sh
@@ -15,7 +15,10 @@ AURA=""
 ETC="/etc"
 SYSD="/${ETC}/systemd"
 SYSDSYS="${SYSD}/system"
+FF_DIR="${HOME}/.mozilla"
 
+mkdir -p "$FF_DIR"
+mkdir -p "$FONT_DIR"
 mkdir -p "$ETC"
 mkdir -p "$SYSD"
 mkdir -p "$SYSDSYS"
@@ -71,11 +74,11 @@ PKGS="${PKGS} bspwm sxhkd"
 AURS="${AURS} polybar"
 AURS="${AURS} picom-ibhagwan-git"
 
-# PKGA="${PKGA} firefox"
-# PKGA="${PKGA} firefox-tridactyl"
-# AURA="${AURA} firefox-tridactyl-native"
-# AURA="${AURA} firefox-ublock-origin"
-# AURA="${AURA} firefox-extension-multi-account-containers"
+PKGA="${PKGA} firefox"
+PKGA="${PKGA} firefox-tridactyl"
+AURA="${AURA} firefox-tridactyl-native"
+AURA="${AURA} firefox-ublock-origin"
+AURA="${AURA} firefox-extension-multi-account-containers"
 
 PKGS="${PKGS} feh"
 PKGS="${PKGS} mpv"
@@ -87,77 +90,41 @@ PKGS="${PKGS} slop"
 PKGS="${PKGS} zathura girara zathura-pdf-mupdf"
 # PKGS="${PKGS} gimp inkscape"
 PKGS="${PKGS} mpd ncmpcpp"
-# AURS="${AURS} ncspot"
+AURS="${AURS} ncspot"
 # AURS="${AURS} spotify"
+# PKGS="${PKGS} alsa-utils"
+# PKGS="${PKGS} pulseaudio pulseaudio-alsa pamixer pavucontrol"
+# PKGS="${PKGS} lmms"
+# PKGS="${PKGS} kdenlive"
+# AURS="${AURS} obs-studio-git"
+# PKGS="${PKGS} linux-headers dkms"
+# PKGA="${PKGA} v4l2loopback-dkms"
 
 # PKGS="${PKGS} hsetroot"
 PKGS="${PKGS} redshift"
 PKGS="${PKGS} ttf-liberation"
 PKGS="${PKGS} adobe-source-code-pro-fonts"
 PKGS="${PKGS} wqy-zenhei"
+# PKGS="${PKGS} breeze"
 PKGS="${PKGS} adapta-gtk-theme"
 PKGS="${PKGS} ttf-roboto"
 
 PKGS="${PKGS} pandoc"
 PKGS="${PKGS} nm-connection-editor"
-
-
-
-
-
-
-# terminal font
-PKGS="${PKGS} libxft"
-# PKGS="${PKGS} kdenlive breeze"
-PKGS="${PKGS} alsa-utils"
-# optional - pulseaudio
-PKGS="${PKGS} pulseaudio pulseaudio-alsa pamixer pavucontrol"
-
-# torrents
 PKGS="${PKGS} aria2"
-
-# qrcodes
 PKGS="${PKGS} qrencode"
-
-# containers
 # PKGS="${PKGS} lxd"
-
-# DAW
-# PKGS="${PKGS} lmms"
-
-# password generator
 # PKGS="${PKGS} pwgen"
 
-# required by xmr-stack (possibly already installed)
-# PKGS="${PKGS} hwloc openssl cmake libmicrohttpd"
-
-# firefox extensions
-
-
-# animated wallpaper eyecandy
-# AUR="${AUR} xwinwrap-git"
-
-# hardened malloc for security (closer to OpenBSD)
-# AUR="${AUR} hardened-malloc-git"
-
-# alt browser
-# AUR="${AUR} nyxt-browser-git"
-
-# streaming/recording/video manipulation
-# AUR="${AUR} obs-studio-git" # git version for latest loopback update
-# PKGS="${PKGS} linux-headers dkms"
-# PKGSA="${PKGSA} v4l2loopback-dkms" # if not already installed
-
 # Unity development
-# https://chrislabarge.com/posts/neovim-unity-engine/
 # if there are still issues finding mono, set the following props
 # "omnisharp.useGlobalMono": "always"
 # "omnisharp.monoPath": "/usr/bin/mono"
-# AUR="${AUR} unityhub"
-# AUR="${AUR} mono-basic" # probably don't need this
+# AURS="${AURS} unityhub"
 # PKGS="${PKGS} mono"
-# PKGS="${PKGS} dotnet-runtime dotnet-sdk" # probably don't need this
-# PKGS="${PKGS} code" # probably don't need this (eventually)
+# probably don't need this
+# AURS="${AURS} mono-basic"
+# PKGS="${PKGS} dotnet-runtime dotnet-sdk"
 
 fi
 
@@ -171,14 +138,6 @@ sudo pacman -S $PKGS
 rustup default stable # rust configuration
 
 for AUR in $AURS; do
-  git clone "https://aur.archlinux.org/${AUR}.git" "${TMPDIR}/${AUR}"
-  cd "${TMPDIR}/${AUR}"
-  makepkg -si
-done
-
-sudo pacman -S $PKGA
-
-for AUR in $AURA; do
   git clone "https://aur.archlinux.org/${AUR}.git" "${TMPDIR}/${AUR}"
   cd "${TMPDIR}/${AUR}"
   makepkg -si
@@ -265,29 +224,20 @@ sudo make install
 touchpadConf="${ETC}/X11/xorg.conf.d/30-touchpad.conf"
 sudo ln -sf "${XDG_CONFIG_HOME}${touchpadConf}" "$touchpadConf"
 
-# TODO
-
 # unmute audio channel
-amixer sset Master unmute
-
-# firefox profile
-mkdir -p "${HOME}/.mozilla"
-ln -sf "${XDG_CONFIG_HOME}/mozilla/firefox" "${HOME}/.mozilla/firefox"
-
-# spotify with spicetify
-sudo chmod a+wr "${OPT_DIR}/spotify"
-sudo chmod a+wr "${OPT_DIR}/spotify/Apps" -R
-yay -S --needed --nocleanmenu --nodiffmenu --noprovides spicetify-cli
-
-# until ncpamixer proves itself to work without segfaulting, alsamixer works just fine...
-# git clone "https://github.com/bossley9/ncpamixer.git" "${TMPDIR}/ncpamixer"
-# cd "${TMPDIR}/ncpamixer"
-# sudo make install
+# amixer sset Master unmute
 
 # alsamixer
-git clone "https://github.com/bossley9/alsamixer.git" "${TMPDIR}/alsamixer"
-cd "${TMPDIR}/alsamixer"
-sudo make clean install
+# git clone "https://github.com/bossley9/alsamixer.git" "${TMPDIR}/alsamixer"
+# cd "${TMPDIR}/alsamixer"
+# sudo make clean install
+
+# spotify with spicetify
+# if command -v "spotify" > "$NULL"; then
+# sudo chmod a+wr "${OPT_DIR}/spotify"
+# sudo chmod -R a+wr "${OPT_DIR}/spotify/Apps"
+# AURA="${AURA} spicetify-cli"
+# fi
 
 # xmr
 # XMR_ARCHIVE="${TMPDIR}/xmr.tar.bz2"
@@ -296,22 +246,23 @@ sudo make clean install
 # tar xvjf "$XMR_ARCHIVE" -C "${TMPDIR}/xmr"
 # cd "${TMPDIR}/xmr"
 # mv "$(ls)" "$XMR_PATH"
-# xmr-stak
-# recommended: https://github.com/fireice-uk/xmr-stak
+# postinstall rec: https://github.com/fireice-uk/xmr-stak
 
-# eww (widgets)
-# git clone "https://github.com/elkowar/eww" "${TMPDIR}/eww"
-# cd "${TMPDIR}/eww"
-# cargo build --release
-# cd "target/release"
-# chmod +x "eww"
-# mv "eww" "${XDG_SCRIPT_HOME}/"
+# firefox profile
+ln -sf "${XDG_CONFIG_HOME}/mozilla/firefox" "${FF_DIR}/firefox"
 
-# font(s)
-mkdir -p "$FONT_DIR"
+# font update
 fc-cache -f -v
 
 fi
+
+sudo pacman -S $PKGA
+
+for AUR in $AURA; do
+  git clone "https://aur.archlinux.org/${AUR}.git" "${TMPDIR}/${AUR}"
+  cd "${TMPDIR}/${AUR}"
+  makepkg -si
+done
 
 # grub
 # timeout
@@ -320,4 +271,4 @@ sudo sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' "$grubConf"
 # regenerate
 sudo grub-mkconfig -o "/boot/grub/grub.cfg"
 
-echo -e "${YELLOW}It is recommended to reboot the system directly after running this sript.${NC}"
+echo -e "${YELLOW}It is recommended to reboot the system directly after running this script.${NC}"
